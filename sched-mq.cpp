e773c33035a4852d982e04283c7d71f78fb6bc2d
Ewan Bloor <80215444+eeweeJr@users.noreply.github.com>
2023-01-14 19:28:29 +0000

    Update sched-mq.cpp

--- sched-mq.cpp
+++ sched-mq.cpp
@@ -1,6 +1,5 @@
 /*
  * The Priority Task Scheduler
- * SKELETON IMPLEMENTATION TO BE FILLED IN FOR TASK 1
  */
 
 #include <infos/kernel/sched.h>
@@ -191,6 +190,5 @@ public:
 
 };
 
-/* --- DO NOT CHANGE ANYTHING BELOW THIS LINE --- */
 
 RegisterScheduler(MultipleQueuePriorityScheduler);
